d4119d59acabe65d4dfe3a04552c48b1ff6bca55
Lappy Mc Lap Face <ephillipsiii>
2018-02-18 18:35:09 -0700

    if/else got jacked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,18 +3,25 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var lettersGuessed = [];
+var userGuess = [];
 
 var computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
+
 document.onkeyup = function(event){
     
-    var userGuess = event.key;
+    
+    var guesses = String.fromCharCode(event.keyCode);
+    userGuess.push(guesses);
 
-    var letter = String.fromCharCode(event.lettersGuessed);
+    for(var i = 0; i++;){
+        userGuess[i] = "_";
+    }
+    
     
     if ((userGuess === computerGuess)){
         wins++;
+        guessesLeft = 10;
         }
         else if ((userGuess !== computerGuess)){
             guessesLeft--;
@@ -24,22 +31,22 @@ document.onkeyup = function(event){
         }
         else if ((losses++) || (wins++) || (guessesLeft = "0")){
             function restart(){
-                document.getElementById(guessesLeft, lettersGuesssed, computerGuess).reset();
+                document.getElementById(guessesLeft, userGuess, computerGuess).reset();
             }
             }
         
-        console.log(userGuess);
         console.log(computerGuess);
         console.log(wins);
         console.log(losses);
         console.log(guessesLeft);
-        console.log(lettersGuessed);
+        console.log(userGuess);
     }
     
 
 var game=
     "<p>Wins: " + wins + "</p>" +
     "<p>Losses: " + losses + "</p>" + 
-    "<p>Guesses Left: " + guessesLeft + "</p>";
+    "<p>Guesses Left: " + guessesLeft + "</p>" +
+    "<p>Letters Guessed: " + [userGuess] + "</p>";
 
 document.querySelector(".game").innerHTML = game;
